f8e1ca87720694b7c13b1ced0f4e8630c59c2c0c
Ефимов Николай <Nikolay.Efimov@redsys.ru>
2017-05-08 20:59:31 +0300

    add controller test

--- src/main/java/bootApp/HelloController.java
+++ src/main/java/bootApp/HelloController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Booot!";
